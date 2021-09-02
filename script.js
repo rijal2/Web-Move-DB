d2de155b5fadd6cb825da7bb88997209e3525573
rijal2 <merentas5benua27@gmail.com>
2021-09-02 15:41:15 +0700

    06. Memasukkan item yang dibutuhkan untuk ditampilkan pada Detail Movies

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ $.ajax({
         // Ketika tombol Lihat di klik
         $('.detail-button').on('click', function(){
             console.log($(this).data('movieid'))
+            $.ajax({
+                url: 'http://www.omdbapi.com/?apikey=c5302d86&i=' + $(this).data('movieid'),
+                success: m => {
+                    const movieDetail = `<div class="container-fluid">
+                                            <div class="row">
+                                            <div class="col-md-3">
+                                                <img src="${m.Poster}" class="img-fluid">
+                                            </div>
+                                            <div class="col-md">
+                                                <ul class="list-group">
+                                                    <li class="list-group-item"><strong>Judul: </strong> ${m.Title} ${m.Year}</li>
+                                                    <li class="list-group-item"><strong>Released: </strong>${m.Released}</li>
+                                                    <li class="list-group-item"><strong>Genre: </strong>${m.Genre}</li>
+                                                    <li class="list-group-item"><strong>Actors: </strong> ${m.Actors}</li>
+                                                    <li class="list-group-item"><strong>Plot: </strong> <br> ${m.Plot}</li>
+                                                    </ul>
+                                            </div>
+                                            </div>
+                                        </div>`;
+                    $('.modal-body').html(movieDetail);
+                },
+                error: (e) => {
+                    console.log(e.responseText);
+                }
+            })
         })
     },
+    error: (e) => {
+        console.log(e.responseText);
+    }
 })
